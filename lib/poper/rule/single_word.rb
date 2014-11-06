4579eeda91314ab783f184247a45c94e0adafc8d
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2014-11-06 20:45:54 +0200

    Test

--- lib/poper/rule/single_word.rb
+++ lib/poper/rule/single_word.rb
@@ -2,6 +2,9 @@ module Poper
   module Rule
     class SingleWord < Rule
       MSG = 'Git commit message should consist of more than a single word'
+      "test test test"
+      "test test test"
+      "test test test"
 
       def check(message)
         MSG if message.split.count < 2
