642d82ede5501aa42c492a72d9c0bd3a3892e7ae
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2017-03-10 15:30:22 +0200

    Randomize specs when running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ require 'rspec/its'
 require 'poper'
 
 RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+
   config.expect_with(:rspec) { |c| c.syntax = :should }
   config.mock_with(:rspec) { |c| c.syntax = :should }
 end
